9576ee01e685c2d22400e6ecb0aa6328ed8d66ff
Geoffrey Parker <professional.cynic@gmail.com>
2010-10-07 16:26:13 -0500

    Part 3 sort of working

--- STFQNWScheduler.java
+++ STFQNWScheduler.java
@@ -16,83 +16,103 @@
  */
 
 import java.util.concurrent.locks.Condition;
+import java.util.*;
 
 public class STFQNWScheduler implements NWScheduler{
 
-  STFQAlarmThread at;
-  SimpleLock mutex;
-  Condition c1;
-  Condition c2; 
-  long CurrentVirtualTime;
-  long maxBW;
-  long nextSecond;
-  long nextTurn;
-  long m;
+	STFQAlarmThread at;
+	SimpleLock mutex;
+	Condition c1;
+	Condition c2; 
+	long CurrentVirtualTime;
+	long maxBW;
+	long nextSecond;
+	long nextTurn;
+	HashMap<Integer,Long> flowFinishTags;
+	int buffId;
 
-  //-------------------------------------------------
-  // Constructor
-  //-------------------------------------------------
-  public STFQNWScheduler(long bytesPerSec)
-  {
-	this.nextTurn = System.currentTimeMillis();
-	this.CurrentVirtualTime = 0;
-	this.maxBW = bytesPerSec; 
-	this.mutex = new SimpleLock();
-	this.c1 = mutex.newCondition();
-	this.c2 = mutex.newCondition();
-    this.at = new STFQAlarmThread(this);
-    this.at.start();
-  }
+	//-------------------------------------------------
+	// Constructor
+	//-------------------------------------------------
+	public STFQNWScheduler(long bytesPerSec)
+	{
+		this.nextTurn = System.currentTimeMillis();
+		this.CurrentVirtualTime = 0;
+		this.maxBW = bytesPerSec; 
+		this.mutex = new SimpleLock();
+		this.c1 = mutex.newCondition();
+		this.c2 = mutex.newCondition();
+		this.flowFinishTags = new HashMap<Integer,Long>();
+		this.at = new STFQAlarmThread(this);
+		this.at.start();
+	}
 
-  //-------------------------------------------------
-  // waitMyTurn -- return only after I may safely
-  // send. If prev send s0 at time t0 transmitted b0
-  // bytes, the next send may not send before
-  // t1 >= t0 + b0/bytesPerSec
-  //
-  // NOTE: See the assignent for important restriction.
-  // In particular, this call must use Condition.await()
-  // (or Condition.awaitUninterruptibly())
-  // with *no arguments* and it may not call Thread.sleep().
-  // Instead, you must rely on an alarmThread to 
-  // signal when it is OK to proceed.
-  //-------------------------------------------------
-  public void waitMyTurn(int flowId, float weight, int lenToSend)
-  {
-	  mutex.lock();
+	//-------------------------------------------------
+	// waitMyTurn -- return only after I may safely
+	// send. If prev send s0 at time t0 transmitted b0
+	// bytes, the next send may not send before
+	// t1 >= t0 + b0/bytesPerSec
+	//
+	// NOTE: See the assignent for important restriction.
+	// In particular, this call must use Condition.await()
+	// (or Condition.awaitUninterruptibly())
+	// with *no arguments* and it may not call Thread.sleep().
+	// Instead, you must rely on an alarmThread to 
+	// signal when it is OK to proceed.
+	//-------------------------------------------------
+	public void waitMyTurn(int flowId, float weight, int lenToSend)
+	{
+		mutex.lock();
+		if(!flowFinishTags.containsKey(flowId))
+			flowFinishTags.put(flowId, (long)0);
+
 		while(System.currentTimeMillis() < nextTurn) {
 			try {
-				//at.run();
 				c1.await();
 			}catch (InterruptedException E) {
 				//do something I guess
 			}
 		}
-		nextTurn = System.currentTimeMillis() + 1000*lenToSend/m;
+		long startTag = Math.max(flowFinishTags.get(flowId),CurrentVirtualTime);  //Guaranteed to be 
+		long finishTag = startTag + (long)(lenToSend / weight);
+		flowFinishTags.put(flowId, finishTag);
+		while(startTag > CurrentVirtualTime) {
+			try {
+				c2.await();
+			}catch (InterruptedException E) {
+				//do something I guess
+			}
+		}
+		nextTurn = System.currentTimeMillis() + 1000*lenToSend/maxBW;
+		if(finishTag > CurrentVirtualTime)
+			CurrentVirtualTime = finishTag;
+		
+
 
 		mutex.unlock();
 		//System.out.println("Sending: " + lenToSend);
-	    return;
-  }
+		return;
+	}
 
 
 
-  //
-  // Hint: you will need to add at least one
-  // new public method that AlarmThread will
-  // call.
-  //
-  public void procede() {
-	  mutex.lock();
-	  c1.signal();
-	  mutex.unlock();
-  }
-  
-  public long getNextTurn(){
-	  mutex.lock();
-	  mutex.unlock();
-	  return nextTurn;
-  }
+	//
+	// Hint: you will need to add at least one
+	// new public method that AlarmThread will
+	// call.
+	//
+	public void procede() {
+		mutex.lock();
+		c1.signal();
+		c2.signal();
+		mutex.unlock();
+	}
+
+	public long getNextTurn(){
+		mutex.lock();
+		mutex.unlock();
+		return nextTurn;
+	}
 
 
 

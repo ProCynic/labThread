29e338a2a6df5758072c6437cf3d4fcd74fd0acd
Alex Chan <aychan@cs.utexas.edu>
2010-10-07 23:21:53 -0500

    adding extra test files

---
+++ DemoScheduler2.java
@@ -0,0 +1,75 @@
+/*
+ * DemoScheduler
+ *
+ * Framework for demonstrate schedulers by sending
+ * lots of bytes through them.
+ *
+ * NOTE: This demo is not an exhaustive test -- it 
+ * does not attempt to test all requirements in the 
+ * specification. Passing these tests is *not*
+ * a guarantee of a good grade on this assignment.
+ *
+ * You must follow the coding standards distributed
+ * on the class web page.
+ *
+ * (C) 2007 Mike Dahlin
+ *
+ */
+import java.util.Random;
+import java.io.IOException;
+
+public class DemoScheduler2{
+
+
+  static int minPort = 4000;
+  static int nPorts = 1000;
+  Source2 source;
+  Sink sink;
+  Stats stats;
+
+  public DemoScheduler2(NWScheduler sched, int nThreads, int bytesToSendPerThread)
+  {
+    int ii;
+    stats = new Stats();
+    int testPort = selectPort();
+    try{
+      sink = new Sink(testPort, false);
+    }
+    catch(IOException e){
+      System.out.println("TestNullScheduler cannot create sink on port " 
+                         + testPort 
+                         + " " + e.toString());
+      e.printStackTrace();
+      System.exit(-1);
+    }
+    float weights[] = new float[nThreads];
+    for(ii = 0; ii < nThreads; ii++){
+      weights[ii] = (float)1.0;
+    }
+    source = new Source2(nThreads, bytesToSendPerThread, sched, 
+                        weights, stats, testPort);
+  }
+
+  public void go(){
+    source.start();
+    source.waitFor();
+    if(sink != null){
+      sink.close();
+    }
+    stats.print();
+  }
+
+  private static int selectPort()
+  {
+    Random r = new Random(System.currentTimeMillis());
+    int rnd = r.nextInt();
+    if(rnd < 0){
+      rnd = rnd * -1;
+    }
+    int port = rnd % nPorts + minPort;
+    assert(port >= minPort);
+    assert(port < minPort + nPorts);
+    return port;
+  }
+
+}
---
+++ Source4.java
@@ -0,0 +1,111 @@
+/*
+ * Source
+ *
+ * Send a bunch of bytes to specified port using
+ * a bunch of threads.
+ *
+ * You must follow the coding standards distributed
+ * on the class web page.
+ *
+ * (C) 2007 Mike Dahlin
+ *
+ */
+import java.net.Socket;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+public class Source4{
+
+	SourceWorker workers[];
+
+	//-------------------------------------------------
+	// Constructor
+	//-------------------------------------------------
+	public Source4(int nFlows, 
+			int bytesToSendPerFlow,
+			NWScheduler sched,
+			float weight[],
+			Stats stats,
+			int testPort)
+	{
+		assert(nFlows < 1000);
+		assert(bytesToSendPerFlow > 1000);
+		assert(weight.length == nFlows);
+		Socket s = null;
+		OutputStream os = null;
+
+		workers = new SourceWorker[2*nFlows];
+
+		int ii;
+		for(ii = 0; ii < nFlows; ii++){
+			try{
+				s = new Socket("localhost", testPort);
+				os = s.getOutputStream();
+				ScheduledOutputStream sos = new ScheduledOutputStream(os,
+						ii,
+						stats,
+						weight[ii],
+						sched);
+				workers[ii] = new SourceWorker(bytesToSendPerFlow, sos);
+				ScheduledOutputStream sos2 = new ScheduledOutputStream(os,
+						ii,
+						stats,
+						weight[ii],
+						sched);
+				workers[(2*nFlows-1)-ii] = new SourceWorker(bytesToSendPerFlow, sos2);
+			}
+			catch(IOException ioe){
+				System.out.println(ioe.toString());
+				ioe.printStackTrace();
+				try{
+					if(os != null){
+						os.close();
+					}
+				}
+				catch(IOException e){
+					// ignore
+				}
+				try{
+					if(s != null){
+						s.close();
+					}
+				}
+				catch(IOException e){
+					// ignore
+				}
+				System.exit(-1);
+			}
+		}
+	}
+
+
+	//-------------------------------------------------
+	// start() -- tell all the worker threads to start
+	//-------------------------------------------------
+	public void start()
+	{
+		int ii;
+		for(ii = 0; ii < workers.length; ii++){
+						
+			workers[ii].start();
+		}
+	}
+
+	//-------------------------------------------------
+	// waitFor() -- join with all threads
+	//-------------------------------------------------
+	public void waitFor()
+	{
+		int ii;
+		for(ii = 0; ii < workers.length; ii++){
+			try{
+				workers[ii].join();
+			}
+			catch(InterruptedException ie){
+				// ignore
+			}
+		}
+	}
+
+}
